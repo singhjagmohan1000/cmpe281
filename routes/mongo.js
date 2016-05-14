06c54849977bf6a0d450e2ff683a3d3499542f55
Jagmohan Singh <Jagmohan.Singh@sjsu.edu>
2016-05-14 07:43:13 -0700

    load-balancer-implemented

--- routes/mongo.js
+++ routes/mongo.js
@@ -1,6 +1,7 @@
 
 //exports.URL ='ec2-52-202-168-18.compute-1.amazonaws.com';
-exports.URL='ec2-52-72-113-55.compute-1.amazonaws.com';
+//exports.URL='ec2-52-72-113-55.compute-1.amazonaws.com';
+exports.URL='Loadbalancerteam7-1989590503.us-east-1.elb.amazonaws.com/';
 exports.PORT=7777;
 /**
  * Connects to the MongoDB Database with the provided URL
